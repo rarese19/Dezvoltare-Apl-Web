a9d98b8fce055fbe3811b4b97668390f2ce3c31a
rarese19 <126828405+rarese19@users.noreply.github.com>
2023-10-12 22:11:58 +0300

    Add files via upload

---
+++ Lab2/WebApplication1/WebApplication1/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public List<Student> students = new List<Student>
+        {
+            new Student { Id = 1, Name = "Ana", Age = 20},
+            new Student { Id = 2, Name = "Maria", Age = 19},
+            new Student { Id = 4, Name = "Vlad", Age = 22},
+            new Student { Id = 5, Name = "Florin", Age = 25},
+            new Student { Id = 3, Name = "Marian", Age = 23}
+        };
+
+        [HttpGet]
+        public List<Student> getAllOrdered()
+        {
+            return students.OrderBy(s => s.Id).ToList();
+        }
+
+        [HttpDelete]
+        public List<Student> DeleteById(int ID)
+        {
+            students.RemoveAll(students => students.Id == ID);
+            return students;
+        }
+    }
+}
