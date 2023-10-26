0f638de508f31a9487f5cad4a02a0565c5e3b2e3
rarese19 <126828405+rarese19@users.noreply.github.com>
2023-10-26 17:28:20 +0000

    Tema 2 DAW

---
+++ Lab3_23/Lab3_23/Migrations/20231026170853_Lab3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab3_23.Migrations
+{
+    /// <inheritdoc />
+    public partial class Lab3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Masini",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Capacitate = table.Column<int>(type: "int", nullable: false),
+                    NumarLocuri = table.Column<int>(type: "int", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Masini", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Masini");
+        }
+    }
+}
---
+++ Lab3_23/Lab3_23/Models/Masina.cs
@@ -0,0 +1,11 @@
+﻿using Lab3_23.Models.Base;
+
+namespace Lab3_23.Models
+{
+    public class Masina: BaseEntity
+    {
+        public string? Marca { get; set; }
+        public int Capacitate { get; set; }
+        public int NumarLocuri {  get; set; }
+    }
+}
